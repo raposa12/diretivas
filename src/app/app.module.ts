fcf6410fce0b85b348ef6e4041d3b679054a61fa
erickson <ericksonjose12@gmail.com>
2022-02-09 14:33:26 -0300

    diretivas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgifComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
